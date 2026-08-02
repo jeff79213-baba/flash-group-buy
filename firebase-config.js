4f8d193d657d0fad97fb8fd1c375ba25fdeff677
馮小高 <jeff79213@gmail.com>
2026-08-02 11:46:35 +0800

    feat: 新增 firebase-config.js 共用設定（正式金鑰）

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCRjAya1_8gZHLSS6ATNOn7xaqD-Vokd7s",
+  authDomain: "flash-group-buy-sk.firebaseapp.com",
+  projectId: "flash-group-buy-sk",
+  storageBucket: "flash-group-buy-sk.firebasestorage.app",
+  messagingSenderId: "632925443741",
+  appId: "1:632925443741:web:5d07353eacf6e20b0f6674"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
